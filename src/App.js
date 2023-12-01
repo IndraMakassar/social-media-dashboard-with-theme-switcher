6be4cef19c117434274277d05105e368b84debc6
Antonius Indra Dharma Prasetya <anindhpra@gmail.com>
2023-12-01 21:37:39 +0800

    finish bigCard

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import {Stack} from "@mui/material";
+import BigCard from "./bigCard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Stack direction={"row"} justifyContent={"space-between"} spacing={4}>
+      <BigCard />
+      <BigCard />
+      <BigCard />
+      <BigCard />
+      </Stack>
   );
 }
 
---
+++ src/bigCard.jsx
@@ -0,0 +1,29 @@
+import {Box, Card, CardContent, Stack, Typography} from "@mui/material";
+import Facebook from "./images/icon-facebook.svg";
+import Up from "./images/icon-up.svg";
+
+function bigCard() {
+    return (
+        <Card variant='outlined' sx={{p: 0, flexGrow: 1}}>
+            {/*<CardContent sx={{p: 0}>*/}
+                <Box component={"div"} sx={{background: 'hsl(208, 92%, 53%)', height: '4px'}}/>
+                <Stack direction="column" alignItems="center" sx={{py: 2}}>
+                    <Stack direction="row" justifyContent='center' alignItems='center' spacing={1}>
+                        <img src={Facebook} alt="Facebook Logo"/>
+                    <Typography>@Nathanf</Typography>
+                    </Stack>
+                <Stack>
+                    <Typography fontWeight="bold" fontSize={60}>1987</Typography>
+                    <Typography textTransform={"uppercase"} letterSpacing={5}>followers</Typography>
+                </Stack>
+                    <Stack direction='row' justifyContent='center' alignItems='center' spacing={1}>
+                        <img src={Up} alt="up"/>
+                    <Typography >12 Today</Typography>
+                    </Stack>
+                </Stack>
+            {/*</CardContent>*/}
+        </Card>
+    );
+}
+
+export default bigCard;
